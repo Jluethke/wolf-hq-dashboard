383d4317b12dc419bd9acb67b005695b590b011a
Jonathan Luethke <jonathanluethke@gmail.com>
2025-04-08 21:49:51 -0400

    Add the actual working React Frontend directly to root

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🐺 Welcome to Wolf HQ Dashboard</h1>
+        <p>This is the frontend live test dashboard!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
