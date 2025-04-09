f6cd08d2c874a31b53233d15d95c2011b922b391
Jonathan Luethke <jonathanluethke@gmail.com>
2025-04-09 10:49:53 -0400

    MVP - Finally?

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,26 @@
-// Updated App.js with real features
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://i.imgur.com/LbSHfMu.png"
+          alt="Wolf HQ Logo"
+          className="App-logo"
+        />
+        <div className="header-text">
+          <div className="header-title">🐺 Welcome to Wolf HQ Dashboard</div>
+          <div className="header-subtitle">The central command for all Warzone missions!</div>
+        </div>
+      </header>
+      <div className="App-body">
+        <p>Test drive the MVP with your squad!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
